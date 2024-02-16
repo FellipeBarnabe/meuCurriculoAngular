01130fe89a4830e08a9017a9f91330b22f54516e
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2024-02-16 14:38:24 -0300

    firstCommitDevelop2

--- meu-curriculo-angular/src/app/components/home/home.component.ts
+++ meu-curriculo-angular/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   providers:[],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
