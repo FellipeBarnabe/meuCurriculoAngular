e613848816697486affac308ca7f08eb246607e4
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2024-02-22 10:26:46 -0300

    thirdCommitDevelop2

--- meu-curriculo-angular/src/app/app.component.spec.ts
+++ meu-curriculo-angular/src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'meu-curriculo-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('meu-curriculo-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, meu-curriculo-angular');
-  });
+  
+});
+it('should have the "meu-curriculo-angular" title', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.componentInstance;
+  expect(app.title).toEqual('meu-curriculo-angular');
 });
