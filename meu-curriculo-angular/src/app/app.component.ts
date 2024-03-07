6de006ef9baf95c980d064e8b1ee5e8890fa62d9
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2024-03-07 10:23:01 -0300

    lastCommitContatc

--- meu-curriculo-angular/src/app/app.component.ts
+++ meu-curriculo-angular/src/app/app.component.ts
@@ -13,5 +13,5 @@ import { ContatcComponent } from './components/contatc/contatc.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'meu-curriculo-angular';
+  title = 'Meu currículo Angular';
 }
