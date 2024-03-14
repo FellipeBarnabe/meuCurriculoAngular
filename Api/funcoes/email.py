9e0cfd48420515a6e561f7321c32d6eb7302e81d
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2024-03-14 19:29:40 -0300

    criando pacote funções

---
+++ Api/funcoes/email.py
@@ -0,0 +1,25 @@
+import smtplib
+import os
+
+from dotenv import load_dotenv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def enviar_email(corpo:str):
+
+    load_dotenv()
+
+    host=os.getenv('HOST_EMAIL')
+    port=os.getenv('PORT_EMAIL')
+    login=os.getenv('LOGIN_EMAIL')
+    senha=os.getenv('SENHA_EMAIL')
+    email_msg= MIMEMultipart()
+    email_msg['Subject'] = 'Formulário de contato do meu currículo'
+    email_msg.attach(MIMEText(corpo,'html','utf-8'))
+    server = smtplib.SMTP(host,port)
+
+    server.ehlo()
+    server.starttls()
+    server.login(login,senha)
+    server.sendmail(login,'fellipebarnabe.stack@gmail.com',email_msg.as_string())
+    server.close()
--- Api/main.py
+++ Api/main.py
@@ -1,24 +1,4 @@
-import smtplib
-import os
+from flask import Flask, request,jsonify
+from funcoes.email import enviar_email
 
-from dotenv import load_dotenv
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-load_dotenv()
-
-host=os.getenv('HOST_EMAIL')
-port=os.getenv('PORT_EMAIL')
-login=os.getenv('LOGIN_EMAIL')
-senha=os.getenv('SENHA_EMAIL')
-email_msg= MIMEMultipart()
-corpo='<h1>FUNCIONOU!</h1>'
-server = smtplib.SMTP(host,port)
-email_msg['Subject'] = 'teste, teste, teste'
-email_msg.attach(MIMEText(corpo,'html'))
-
-server.ehlo()
-server.starttls()
-server.login(login,senha)
-server.sendmail(login,'fellipebarnabe.stack@gmail.com',email_msg.as_string())
-server.close()
+enviar_email("<h1>Ola!</h1>")
