38d8b95787b69544aebf1aa321d3a74e4526233a
Marcone Melo Mendonça <marconemendonca@ymail.com>
2024-03-14 22:49:21 -0300

    melhorando a logica de validação do body

--- Api/main.py
+++ Api/main.py
@@ -1,35 +1,36 @@
 import json
 from jsonschema import validate
-from flask import Flask, request,jsonify
+from flask import Flask, request, jsonify
 from funcoes.email import enviar_email
 from http import HTTPStatus
 
-#enviar_email("<h1>Ola!</h1>")
+# enviar_email("<h1>Ola!</h1>")
 
 app = Flask(__name__)
 
 
-@app.route("/processaFormulario", methods = ['POST'])
+@app.route("/processaFormulario", methods=["POST"])
 def xxx():
     data = request.get_json()
 
-    if not data: 
+    if not data:
         return jsonify({"erro": "body não informado."}), HTTPStatus.BAD_REQUEST
-    
-    if hasattr(data, "nome"):
-        if hasattr(data, "email"):
-            if hasattr(data, "message"):
-                return jsonify({"nome":"Fellipe","idade": 34})
-                
-            return jsonify({"erro": "O atributo \"message\" não foi informado."}), HTTPStatus.UNPROCESSABLE_ENTITY
-        
-        return jsonify({"erro": "O atributo \"email\" não foi informado."}), HTTPStatus.UNPROCESSABLE_ENTITY
-    
-    return jsonify({"erro": "O atributo \"nome\" não foi informado."}), HTTPStatus.UNPROCESSABLE_ENTITY
 
-    
-    
-    
+    erros = []
+
+    if not "nome" in data:
+        erros.append('O atributo "nome" não foi informado.')
+
+    if not "email" in data:
+        erros.append('O atributo "email" não foi informado.')
+
+    if not "message" in data:
+        erros.append('O atributo "message" não foi informado.')
+
+    if erros:
+        return jsonify({"erros": erros}), HTTPStatus.UNPROCESSABLE_ENTITY
+
+    return jsonify({"nome": "Fellipe", "idade": 34})
 
 
 app.run(debug=True)
