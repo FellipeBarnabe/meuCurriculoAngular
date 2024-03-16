0d4e68ff39978623b1f4a23d1d97674814951cce
Marcone Melo Mendonça <marconemendonca@ymail.com>
2024-03-16 18:16:36 -0300

    Melhoria no template HTML

--- Api/models/Email.py
+++ Api/models/Email.py
@@ -1,13 +1,18 @@
 class Email:
 
-    def __init__(self, name: str, email: str, msg: str):
+    def __init__(self, name: str, sender: str, msg: str):
         self.name = name
-        self.sender = email
+        self.sender = sender
         self.msg = msg
 
     def get_html_message(self) -> str:
-        html = f"<header><b>De: </b><i>{self.sender}</i></header><hr>"
-        html += f"<h3>Caro, Fellipe</h3><p>O(a) <b>{self.name}</b> te mandou a seguinte mensagem:</p>"
-        html += f"<p><i>\"{self.msg}\"</i></p>"
-
-        return html
+        return f"""
+        <header>
+            <b>De:</b> <i>{self.sender}</i>
+        </header>
+        <hr>
+        <h3>Caro Fellipe,</h3>
+        <p>O(A) <b>{self.name}</b> te mandou a seguinte mensagem:</p>
+        <p><i>"{self.msg}"</i></p>
+        """
+    
