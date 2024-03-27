dcf20f7a8a7f56a374c4bc148bd402475e19f84e
Marcone Melo Mendonça <marconemendonca@ymail.com>
2024-03-27 01:40:23 -0300

    refactor: melhoria no 'secondary-button' component

--- src/app/components/secondary-button/secondary-button.component.spec.ts
+++ src/app/components/secondary-button/secondary-button.component.spec.ts
@@ -1,23 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Route } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { SecondaryButtonComponent } from './secondary-button.component';
 
-describe('SecundaryButtonComponent', () => {
+fdescribe('SecundaryButtonComponent', () => {
   let component: SecondaryButtonComponent;
   let fixture: ComponentFixture<SecondaryButtonComponent>;
+  let routerTest: RouterTestingModule;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SecondaryButtonComponent]
-    })
-    .compileComponents();
-    
+      imports: [SecondaryButtonComponent, RouterTestingModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SecondaryButtonComponent);
     component = fixture.componentInstance;
+    routerTest = fixture.debugElement.injector.get(RouterTestingModule);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('CT001 - should be created', () => {
     expect(component).toBeTruthy();
   });
 });
